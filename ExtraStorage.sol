4a75febc18194655ecc297273af4f3af45bbc8ee
r2-col <108417620+r2-col@users.noreply.github.com>
2022-08-25 13:08:59 -0700

    Update ExtraStorage.sol

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.8;
 
-import "./simplesol.sol";
+import "./simplestorage.sol";
 
 contract ExtraStorage is SimpleStorage {
     function store(uint256 _favoriteNumber) public override {
