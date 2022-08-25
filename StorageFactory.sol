cb9efe3d746cb0d3cc7c0dfa814955d84c8ce2be
r2-col <108417620+r2-col@users.noreply.github.com>
2022-08-25 12:00:39 -0700

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,20 +2,26 @@
 
 pragma solidity ^0.8.12;
 
-import "./simplesol.sol"; 
+import "./simplestorage.sol"; 
 
 contract StorageFactory {
     
-    SimpleStorage[] public simpleStorageArray;
+    simplestorage[] public simpleStorageArray;
     
-    function createSimpleStorageContract() public {
-        SimpleStorage simpleStorage = new SimpleStorage();
+    function createsimplestoragesontract() public {
+        simplestorage simpleStorage = new simplestorage();
         simpleStorageArray.push(simpleStorage);
     }
     
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // Address 
         // ABI 
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        // simplestorage(address(simplestorageArray[_simplestorageIndex])).store(_simplestorageNumber);
         simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
